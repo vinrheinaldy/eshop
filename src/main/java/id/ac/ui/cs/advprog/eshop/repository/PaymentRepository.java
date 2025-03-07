cacd1305e71e5c7bafed73305f1832f11adbd41c
Alvin <imoonfellow@gmail.com>
2025-03-07 21:15:06 +0700

    [RED] Payment Service Skeletons

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface PaymentRepository {
     Payment create(Payment car);
     List<Payment> findAll();
