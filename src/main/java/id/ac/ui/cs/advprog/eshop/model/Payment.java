562148018bc3f2cb652947f1ba2518a3a59801ed
Alvin <imoonfellow@gmail.com>
2025-03-07 19:08:15 +0700

    [RED] Add skeleton for payment.

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+    private Order order;
+
+    public Payment(String id, String method, String status, Map<String, String> paymentData, Order order) {
+    }
+
+}
